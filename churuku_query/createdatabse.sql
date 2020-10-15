32da2044dbc20dbf8b1eb83e8fd29805d1f69b40
yunruowu <2513210559@qq.com>
2020-10-15 14:15:38 +0800

    项目增加了new文件夹，包括原有的项目需求和数据库和表的创建文件

---
+++ churuku_query/createdatabse.sql
@@ -0,0 +1,19 @@
+CREATE database churuku;
+use churuku;
+set character_set_client = utf8;
+-- 创建表
+CREATE TABLE IF NOT EXISTS `cailiao`(
+   `runoob_id` INT UNSIGNED AUTO_INCREMENT,
+   `runoob_title` VARCHAR(100) NOT NULL,
+   `runoob_author` VARCHAR(40) NOT NULL,
+   `submission_date` DATE,
+   PRIMARY KEY ( `runoob_id` )
+)DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `kucun`(
+   `runoob_id` INT UNSIGNED AUTO_INCREMENT,
+   `runoob_title` VARCHAR(100) NOT NULL,
+   `runoob_author` VARCHAR(40) NOT NULL,
+   `submission_date` DATE,
+   PRIMARY KEY ( `runoob_id` )
+)CHARSET=utf8;
